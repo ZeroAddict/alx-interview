645d1f03e07e8f7a717403cb475d145eb9ad7dc1
ZeroAddict <yemifrancisolayemi@gmail.com>
2024-10-06 13:37:51 -0700

    BEtter at list comprehension than I started

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    k reps PascalTriangleList
+    inner for loop specifies a j index for number of elem
+    to be inserted at 1th  index of current row
+    adds the previous rows's jth and j+1th
+    Args:
+        n (int): number of rows
+    """
+    k = []
+    if n <= 0:
+        return k
+    k = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            row.append(k[i - 1][j] + k[i - 1][j + 1])
+            row.append(1)
+        k.append(row)
+    return k
