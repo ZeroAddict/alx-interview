f49543defd94fe2a7768fc44537b3fe6923ddf99
ZeroAddict <yemifrancisolayemi@gmail.com>
2024-10-12 14:04:30 -0700

    corrected pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 0-pascal_triangle
-This module contains a function that returns a list of lists of integers representing the Pascal’s triangle of n:
+This module contains a function that returns a list of lists
+of integers representing the Pascal’s triangle of n:
 """
 
 
@@ -9,13 +10,12 @@ def pascal_triangle(n):
     """
     Returns a list of lists of integers
     representing the Pascal Triangle of n
-    
     returns empty list if n <= 0
     k reps PascalTriangleList
     inner for loop specifies a j index for number of elem
     to be inserted at 1th  index of current row
     adds the previous rows's jth and j+1th
-    
+
     Args:
         n (int): number of rows
     """
@@ -25,7 +25,7 @@ def pascal_triangle(n):
     k = [[1]]
     for i in range(1, n):
         row = [1]
-        for j in range(1, len(k[i - 1])): 
+        for j in range(1, len(k[i - 1])):
             row.append(k[i - 1][j - 1] + k[i - 1][j])
         row.append(1)
         k.append(row)
