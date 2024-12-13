e9d4f76bc1f196b952801cea56bd1b008bf65437
ZeroAddict <yemifrancisolayemi@gmail.com>
2024-12-13 02:58:38 +0100

    alx interview primegame

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+def determine_prime_game_winner(player_turns, numbers):
+    """
+    Determine the winner of the prime game.
+
+    Args:
+        player_turns (int): The number of turns in the game.
+        numbers (list): A list of numbers for each turn.
+
+    Returns:
+        str: The winner of the game, either 'Maria' or 'Ben', or None if it's a tie.
+    """
+
+    if player_turns < 1 or not numbers:
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+
+    # Generate a list of prime numbers up to the maximum number in the input list
+    max_number = max(numbers)
+    prime_numbers = [True] * (max_number + 1)
+    prime_numbers[0] = prime_numbers[1] = False
+
+    for potential_prime in range(2, int(max_number ** 0.5) + 1):
+        if prime_numbers[potential_prime]:
+            for multiple in range(potential_prime ** 2, max_number + 1, potential_prime):
+                prime_numbers[multiple] = False
+
+    # Count the number of prime numbers less than each input number
+    for num in numbers:
+        prime_count = sum(prime_numbers[2:num + 1])
+        if prime_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins == ben_wins:
+        return None
+    return 'Maria' if maria_wins > ben_wins else 'Ben'
